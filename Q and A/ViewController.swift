cbef2a8340cdf05bbe02cd29cb5a6032b3388666
Pin Huang <justmailmason@gmail.com>
2020-09-07 13:34:47 +0800

    Initial Commit

---
+++ Q and A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q and A
+//
+//  Created by Pincheng Huang on 2020/9/7.
+//  Copyright © 2020 Pincheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
